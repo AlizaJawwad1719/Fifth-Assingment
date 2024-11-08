44a9e6cbb3eeb53c04ca65dbbaf46393fb52cdfd
AlizaJawwad1719 <159164016+AlizaJawwad1719@users.noreply.github.com>
2024-11-09 02:12:33 +0500

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,22 @@
+
+function Header () {
+    return (
+      <div className="h-[150px] w-[1920px] p-4 flex" style={{ backgroundColor:'rgb(162, 152, 117)'}}>
+        <h1 className="w-[439px] h-[94px] font-bold text-white flex p-10 text-[35px]"> MANZARRI</h1>
+  
+        <div className='flex gap-4 items-center justify-center'>
+          <input type='text'
+            placeholder='Search for Gold & Diamond Jewellery and many more things...'
+            className='p-4 rounded-lg text-black-950 w-[769.89px]  h-[56.24px]'>
+
+          </input>
+        </div>
+
+
+      </div>
+      
+    )
+  }
+  
+  export default Header
+  
